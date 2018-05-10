7fcb338e0cc88a62268023b6e98b1b262a177e06
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2018-05-10 20:51:07 +0200

    using SuGOku API
    sugoku API still not accepting input, it has to be formatted differently. sugoku API also provides ambiguous puzzles

--- main.py
+++ main.py
@@ -2,22 +2,28 @@
 # -*- coding: UTF-8 -*-
 #python 3.6.4
 
-# import pandas as pd
-# import numpy as np
-# import matplotlib
-#import codecs
-import itertools
-#import requests
+#unit rule (only possible in that cell of the unit)
+#intersection
+#http://www.sudokuslam.com/hints.html
 
-#APIlink='https://sugoku.herokuapp.com/board?difficulty=easy'
+import itertools
+import requests
+import sys
+import logging
+import time
+import json
+
+logging.basicConfig(level=logging.DEBUG)
+
+APIlink="https://sugoku.herokuapp.com/board?difficulty=easy"
 
 allPossibilities = list('123456789')
 
 class Cell():
 	def __init__(self,value,locationY,locationX):
-		self.location = (locationY,locationX)
+		self.location = (locationX,locationY)
 		self.possibilities = list(allPossibilities)
-		self.value = value
+		self.value = str(value)
 		self.square = squareFinder(self.location)
 		self.solved = False
 
@@ -51,30 +57,6 @@ class Cell():
 			self.value = self.possibilities[0]
 		return
 
-# class Unit():
-# 	def __init__(self,unitType,cellsList):
-# 		if unitType in ['row','col','square']:
-# 			self.unitType = unitType
-# 		else:
-# 			raise ValueError("type of unit incompatible")
-# 		if len(cellsList) != 9:
-# 			raise ValueError("not enough cells in unit!")
-# 		self.cellsList = cellsList
-# 		self.valid = False
-# 		self.testValidity()
-
-# 	def testValidity(self):
-# 		count = 0
-# 		runOn = []
-# 		for cell in cellsList:
-# 			count += int(cell.value)
-# 			runOn.append(int(cell.value))
-# 		if count != 45:
-# 			raise AssertionError("sum doesn't equal 45")
-# 		if sorted(runOn) != [1,2,3,4,5,6,7,8,9]:
-# 			raise AssertionError("doesn't contain 1-9")
-# 		self.valid = True
-
 def squareFinder(location):
 	squareID = 0
 	if location in itertools.product(range(0,3),range(0,3)):
@@ -97,30 +79,45 @@ def squareFinder(location):
 		squareID = 9
 	return squareID
 
-samplePuzzle = [
-	['0','3','9','1','0','6','2','0','0'],
-	['6','0','4','5','0','0','0','0','0'],
-	['0','5','0','0','2','0','0','6','0'],
-	['0','1','2','0','9','0','5','4','0'],
-	['9','0','0','0','0','0','0','0','1'],
-	['0','4','7','0','3','0','8','2','0'],
-	['0','6','0','0','7','0','0','9','0'],
-	['0','0','0','0','0','3','1','0','4'],
-	['0','0','3','9','0','4','6','8','0']
-	]
+logging.debug("getting puzzle")
+
+page = requests.get(APIlink)
+if page.status_code != 200:
+	sys.exit()
+
+logging.debug("puzzle acquired")
+
+print(page.text)
+
+sudokuPuzzle = page.json()['board']#[list('000000000'),list('080050030'),list('302009504'),list('400000200'),list('060080090'),list('001000003'),list('506800402'),list('010090070'),list('000000000')]#
+
+logging.debug("puzzle: %s"%sudokuPuzzle)
+
+#print(json.dumps({'board':sudokuPuzzle}))
+onlineSolution = requests.post('https://sugoku.herokuapp.com/grade',page.text)
+print(onlineSolution.text)
+sys.exit()
 
 allCellsList = []
 
 rowNumber = 0
-for row in samplePuzzle:
+for row in sudokuPuzzle:
 	colNumber = 0
 	for number in row:
 		allCellsList.append(Cell(number,colNumber,rowNumber))
 		colNumber +=1
 	rowNumber +=1
 
+logging.debug("starting solve")
+startTime = time.time()
+
 notSolved = True
 while notSolved:
+	if time.time()-startTime >= 3:
+		logging.debug("taking too long, aborting")
+		for cell in allCellsList:
+			print(cell.value,cell.location,cell.square,cell.possibilities)
+		sys.exit()
 	notSolved = False
 	for cell in allCellsList:
 		cell.calculatePossibilities(allCellsList)
@@ -128,6 +125,26 @@ while notSolved:
 		if cell.solved == False:
 			notSolved = True
 
-print("solved")
+logging.debug("solved")
+solutionDict = {0:[],1:[],2:[],3:[],4:[],5:[],6:[],7:[],8:[]}
 for cell in allCellsList:
-	print(cell.value,cell.location)
+	solutionDict[cell.location[0]].append(int(cell.value))
+	#print(cell.value,cell.location)
+
+print(solutionDict, "\n")
+
+solution = []
+
+for i in range(0,9):
+	solution.append(solutionDict[i])
+
+print(solution)
+
+data = {'board':solution}
+print(json.dumps(data))
+response = requests.post('https://sugoku.herokuapp.com/validate',json.dumps(data))
+
+print(response.text)
+
+onlineSolution = requests.post('https://sugoku.herokuapp.com/solve',json.dumps({'board':sudokuPuzzle}))
+print(onlineSolution.text)
